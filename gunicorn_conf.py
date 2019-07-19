8b94bbd28ff33db93db5516cce556214e64730e5
liwb <li.wenbo2@qq.com>
2019-07-19 10:57:59 +0800

    modified gunicon to run the app

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -4,8 +4,7 @@ import multiprocessing
 pidfile = 'gun.pid'
 backlog = 512
 bind = "0.0.0.0:8888"
-daemon = True
-reload = True
+#daemon = True
 
 # log
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(L)s ' \
